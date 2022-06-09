11b3150650042bc13481ab68c68d3a3b3b00e9e5
Kestutis M <pampyras@gmail.com>
2022-06-09 13:09:39 +0300

    Added logging to closure

--- examples/get-balances.php
+++ examples/get-balances.php
@@ -11,6 +11,8 @@ use Posti\Glue\Api;
 try {
     $api = new Api($username, $password, $business_id, $contract_number, true);
     $api->setDebug(true, "../debug.log");
+    //log to function
+    //$api->setDebug(true, static function($message) { var_dump($message); });
     $balances = $api->getBalances();
     echo '<pre>';
     var_dump($balances);
